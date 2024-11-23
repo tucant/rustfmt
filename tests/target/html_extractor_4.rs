6c40894c2a71cff2637eaf515af3e2347c395d0d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-11-23 19:55:05 +0100

    broken case

---
+++ tests/target/html_extractor_4.rs
@@ -0,0 +1,5 @@
+pub fn main() {
+    html_extractor::html!(
+                <a class=_linkclass href={&format!("/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N{id:015},-N000464,-Avvarchivstart%2Ehtml")}>"Archiv"</a>
+    );
+}
