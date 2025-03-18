97bfaebf6254f8a62b9c5144e8d99940f03e7f72
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-03-18 23:27:25 +0100

    Expected result

---
+++ tests/source/html_extractor_14.rs
@@ -0,0 +1,14 @@
+pub fn main() {
+    html_extractor::html! {
+<div>
+</div>
+let location_or_additional_info = if html_handler.peek().is_some() {
+let location_or_additional_info = html_handler.next_any_child();
+</p>_
+} => location_or_additional_info; else {
+</p>_
+} => ();
+<div>
+</div>
+    };
+}
